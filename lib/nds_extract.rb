f846792db442d899ca91439ba8e3dbaf9c986642
Taiye Salami <taiye41@gmail.com>
2020-02-25 12:59:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,34 +4,8 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 
-director_data = {:name=>"Stephen Spielberg",
-  :movies=>
-  [{:title=>"Jaws",
-    :studio=>"Universal",
-    :worldwide_gross=>260000000,
-    :release_year=>1975},
-    {:title=>"Close Encounters of the Third Kind",
-    :studio=>"Columbia",
-    :worldwide_gross=>135189114,
-    :release_year=>1977},
-    {:title=>"Raiders of the Lost Ark",
-    :studio=>"Paramount",
-    :worldwide_gross=>248159971,
-    :release_year=>1981},
-    {:title=>"E.T. the Extra-terrestrial",
-    :studio=>"Universal",
-    :worldwide_gross=>435110554,
-    :release_year=>1982},
-    {:title=>"Schindler's List",
-    :studio=>"Universal",
-    :worldwide_gross=>96898818,
-    :release_year=>1993},
-    {:title=>"Lincoln",
-    :studio=>"Buena Vista",
-    :worldwide_gross=>182207973,
-    :release_year=>2012}]}
-    
-    
+require 'director_data'
+
 def gross_for_director(director_data)
   total = 0 
   
